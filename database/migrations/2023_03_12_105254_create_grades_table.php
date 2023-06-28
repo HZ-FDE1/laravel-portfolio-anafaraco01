779d23b5758cfd6221d5cab1841fc9c0915f71ba
dwaard <daan.de.waard@hz.nl>
2023-06-28 15:56:27 +0200

    Set default value to weighing_factor attribute

--- database/migrations/2023_03_12_105254_create_grades_table.php
+++ database/migrations/2023_03_12_105254_create_grades_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
             $table->string('test_name');
-            $table->float('weighing_factor');
+            $table->float('weighing_factor')->default(1.0);
             $table->decimal('lowest_passing_grade', 3, 1)->default(5.5)->comment('Lowest grade to pass so average calculations can be applied');
             $table->decimal('best_grade', 3, 1)->nullable();
             $table->timestamps();
